b86de283d48615061a411fb783efedc213410fc9
Shubham-tiwari123 <st540362@gmail.com>
2020-01-11 20:31:45 +0530

    Client Side API

---
+++ src/com/project/client/requestAPI/TransferBlockReqAPI.java
@@ -0,0 +1,53 @@
+package com.project.client.requestAPI;
+
+import com.project.client.entity.ClientKeys;
+import com.project.client.services.TransferBlock;
+import org.json.simple.JSONObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet(name = "TransferBlockReqAPI", urlPatterns = {"/send_block"})
+public class TransferBlockReqAPI extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        try {
+            TransferBlock transferBlock = new TransferBlock();
+            PrintWriter writer = response.getWriter();
+            // Prepare block
+            String productQun = request.getParameter("productQun");
+            String productName = request.getParameter("productName");
+            long quantity = Long.parseLong(productQun);
+
+            String blockString = transferBlock.prepareBlock(quantity, productName);
+            String currentBlockHash = transferBlock.calBlockHash(blockString);
+
+            blockString = transferBlock.manipulateBlock(blockString,currentBlockHash);
+
+            ClientKeys keys = transferBlock.getKeysFromDatabase();
+            ArrayList<byte[]> encryptedBlock = transferBlock.encryptBlock(keys,blockString);
+
+            // attach the signature of client signature
+
+            JSONObject result = new JSONObject();
+            result.put("block",blockString);
+            result.put("hash",currentBlockHash);
+
+            writer.println(result.toJSONString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
