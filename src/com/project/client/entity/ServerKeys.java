eabf49fbbdd162dea566494767d315017e76f39c
Shubham-tiwari123 <st540362@gmail.com>
2020-01-14 21:26:41 +0530

    Client Side API

---
+++ src/com/project/client/entity/ServerKeys.java
@@ -0,0 +1,26 @@
+package com.project.client.entity;
+
+import java.math.BigInteger;
+
+public class ServerKeys {
+
+    private BigInteger publicKeyModules ;
+    private BigInteger publicKeyExpo ;
+
+    public BigInteger getPublicKeyModules() {
+        return publicKeyModules;
+    }
+
+    public void setPublicKeyModules(BigInteger publicKeyModules) {
+        this.publicKeyModules = publicKeyModules;
+    }
+
+    public BigInteger getPublicKeyExpo() {
+        return publicKeyExpo;
+    }
+
+    public void setPublicKeyExpo(BigInteger publicKeyExpo) {
+        this.publicKeyExpo = publicKeyExpo;
+    }
+
+}
