843baaa2fc3eb9288c0fb95910d21312ed4c008e
Shubham-tiwari123 <st540362@gmail.com>
2020-01-14 21:27:20 +0530

    Client Side API

---
+++ src/com/project/client/utils/VariableClass.java
@@ -0,0 +1,14 @@
+package com.project.client.utils;
+
+public final class VariableClass {
+    final static public String DATABASE_NAME="client_pc_db";
+    final static public String STORE_KEYS="storeKeys";
+    final static public String IP_ADDRESS="127.0.0.1";
+    final static public int PORT_NUMBER = 27017;
+
+    final static public int SUCCESSFUL= 200;
+    final static public int FAILED = 400;
+    final static public int BAD_REQUEST=401;
+    final static public int INTERNAL_SERVER_ERROR=500;
+
+}
